00815d02b802292ad87b9f4cf65dff977ba0d811
Masahiro Ishii <marufeuille@gmail.com>
2019-03-22 20:21:44 +0800

    use unix domain socket

--- src/init.sh
+++ src/init.sh
@@ -26,13 +26,10 @@ cd /var/www/html
 npm install xterm
 mv /tmp/index.html .
 
-mkdir -p /etc/systemd/system/docker.service.d/
-mv /tmp/override.conf /etc/systemd/system/docker.service.d/
-systemctl daemon-reload
 systemctl restart docker
-
 docker run -itd --name websocket alpine:latest /bin/ash
 
 mv /tmp/websocket.conf /etc/nginx/conf.d/
 rm -f /etc/nginx/sites-enabled/default
+usermod -aG docker www-data
 systemctl restart nginx
